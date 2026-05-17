151b2d11ffe30b80a218b815784d9211304e839a
MD Mahabub Alam Bishal <mdmahabubalambishal@gmail.com>
2026-05-17 23:30:26 +0600

    Remove sw fetch handler

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -2,13 +2,6 @@ self.addEventListener('install', () => {
   self.skipWaiting()
 })
 
-self.addEventListener('activate', () => {
-  self.clients.claim()
-})
-
-self.addEventListener('fetch', event => {
-  // Only handle same-origin requests
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(fetch(event.request))
-  }
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim())
 })
