90a13541beacc5f6ce2e106d145d5359b7bbcea6
MD Mahabub Alam Bishal <mdmahabubalambishal@gmail.com>
2026-05-17 23:43:24 +0600

    Fix CSP for fonts

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -8,7 +8,14 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "font-src 'self' fonts.gstatic.com fonts.googleapis.com data:;",
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline'",
+              "style-src 'self' 'unsafe-inline' fonts.googleapis.com",
+              "font-src 'self' fonts.gstatic.com fonts.googleapis.com data: https://k2mkucxia43oc7fa.public.blob.vercel-storage.com",
+              "img-src 'self' data: blob: https:",
+              "connect-src 'self' https: wss:",
+            ].join('; '),
           },
         ],
       },
